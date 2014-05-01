2b967f69066f1969597527004ebfcbb13c62ece1
evolily <evolily@gmail.com>
2014-05-02 01:26:01 +0800

    Take into account phase delay produced by damping filter

--- getdelaylen.m
+++ getdelaylen.m
@@ -1,7 +1,8 @@
-function [D, d] = getdelaylen(f, fs)
-%GETDELAYLEN  Returns integer delay line length `D` and fractional delay 
-%   line length `d` based on desired frequency `f` and sampling frequency
-%   `fs`. `d` should be within the range of [0.1, 1.1] for stability.
+function [D, d] = getdelaylen(f, fs, dp_b, dp_a)
+%GETDELAYLEN  Returns integer delay line length D and fractional delay 
+%   line length d based on desired frequency f, sampling frequency
+%   fs, and damping filter coefficients dp_b and dp_a. d should be within 
+%   the range of [0.1, 1.1] for stability.
 %
 %   Ref: Smith, J. O. (2005). MUS420/EE367A Lecture 4A, Interpolated Delay 
 %   Lines, Ideal Bandlimited Interpolation, and Fractional Delay Filter 
@@ -9,9 +10,14 @@ function [D, d] = getdelaylen(f, fs)
 %   Stanford University, Stanford, CA. Retrieved from 
 %   https://ccrma.stanford.edu/~jos/Interpolation/Interpolation_4up.pdf
 
-D = floor(fs/f);
-d = fs/f - D;
+% Get phase delay produced by damping filter.
+phi = phasedelay(dp_b, dp_a, [0 f], fs);
+phi = phi(end) * fs / (2*pi);   % convert to samples
 
+D = floor(fs/f - phi);
+d = fs/f - phi - D;
+
+% 0.1 < d < 1.1
 if d < 0.1
     d = d + 1;
     D = D - 1;
