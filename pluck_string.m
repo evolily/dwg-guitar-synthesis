de6743c9a16b2a52f876144252580d33c963a2cf
evolily <evolily@gmail.com>
2014-05-02 01:32:37 +0800

    Rewrite getdelaylen.m to generate correct fractional delay length

--- pluck_string.m
+++ pluck_string.m
@@ -2,23 +2,23 @@ close all; clear all; clc
 
 %% Constant
 LEN = 3;    % length of note (sec)
-FREQ = 110; % frequency of note (Hz)
+FREQ = 261.626; % frequency of note (Hz)
 
 %% Read Body Response File
 [ex, fs] = wavread('gtrbody.wav');  % excitation signal
 
+%% Design Damping Filter Hd(z)
+% Use a one-pole IIR to simulate damping filter.
+[dp_b, dp_a] = dampingfilter;
+
 %% Get Delay Line Length
 % D is for integer delay line z^(-D).
 % d is for fractional delay F(z).
-[D, d] = getdelaylen(FREQ, fs);
+[D, d] = getdelaylen(FREQ, fs, dp_b, dp_a);
 
 %% Design Fractional Delay F(z)
 % Use 1st-order allpass as fractional delay.
 [fr_b, fr_a] = fractionaldelay(d);
-
-%% Design Damping Filter Hd(z)
-% Use a one-pole IIR to simulate damping filter.
-[dp_b, dp_a] = dampingfilter;
 
 %% Build String Filter S(z) by Consolidating Filters into B(z)/A(z) Form
 % Consolidate damping filter and fractional delay.
@@ -39,17 +39,19 @@ x = [ex; zeros(LEN*fs-length(ex), 1)];
 y = filter(b, a, x);
 
 % Normalize y
-y = y/(abs(max(y))+.1);
+y = y/(max(abs(y))+.1);
 
 %% Plot Magnitude Spectrum
-Y = fft(y)/(LEN*fs);
-Ymag = 20*log10(abs(Y(1:LEN*fs/2+1)));
-F = fs/2 * linspace(0, 1, LEN*fs/2+1);
+nfft = 8 * LEN * fs;
+Y = fft(y, nfft)/(LEN*fs);
+Ymag = 20*log10(abs(Y(1:nfft/2+1)));
+F = fs/2 * linspace(0, 1, nfft/2+1);
 
 figure; plot(F, Ymag)
 xlabel('Frequency (Hz)')
 ylabel('Magnitude (dB)')
-xlim([0 5000])
+xlim([0 1000])
+datacursormode on
 
 %% Save Synthetic File
 wavwrite(y, fs, 'synthetic.wav')
